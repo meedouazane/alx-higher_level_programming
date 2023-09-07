b57d6767cf0f46505486dbf44819c7fec583a9fc
meedouazane <mohamedouazane@yahoo.com>
2023-09-07 04:27:19 -0700

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    result = hidden_4.some_function()
     for i in dir(hidden_4):
-        if i != "_":
+        if i[0] != "_":
             print("{}".format(i))
