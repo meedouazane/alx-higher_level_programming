ea7dd553ee3da82ed4e1105df8e19008474dd128
meedouazane <mohamedouazane@yahoo.com>
2023-09-07 03:48:13 -0700

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    i = 1
+    if argc == 0:
+        print("0 argument.")
+    elif argc == 1:
+        print("1 argument.")
+    else:
+        print("{} arguments.".format(argc))
+    for i in range(i, argc + 1):
+        print("{}: {}".format(i, sys.argv[i]))
