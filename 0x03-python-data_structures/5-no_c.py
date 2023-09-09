b93da5e271d127fd544796ce7369a2ffa1c9f936
meedouazane <mohamedouazane@yahoo.com>
2023-09-09 06:18:32 -0700

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     new = ""
     length = len(my_string)
     for i in range(length):
-        if my_string[i] != 'C' and my_string[i] != 'c':
+        if (my_string[i] != 'C' and my_string[i] != 'c'):
             new = new + my_string[i]
     return(new)
