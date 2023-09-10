70bfad6a3d84b4248f5a60610e30ab37b5b84080
meedouazane <mohamedouazane@yahoo.com>
2023-09-10 04:57:29 -0700

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return (''.join(char for char in my_string if char.lower() != 'c' and char.upper() != 'C'))
+    return (''.join(char for char in my_string if char != 'c' and char != 'C'))
