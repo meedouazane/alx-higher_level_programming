4ebf42206863ff81651c7564dd2192aa89cb3bf8
meedouazane <mohamedouazane@yahoo.com>
2023-09-10 04:55:23 -0700

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ""
-    for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'C'):
-            new += my_string[i]
-    return(new)
+    return (''.join(char for char in my_string if char.lower() != 'c' and char.upper() != 'C'))
