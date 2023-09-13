f149d7d204ec277af72564023f10a63a5351a25d
meedouazane <mohamedouazane@yahoo.com>
2023-09-13 07:43:30 -0700

    12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    roman = {'I' : 1, 'V': 5,'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    number = 0
+    prev = 0
+    for i in roman_string[::-1]:
+        value = roman[i]
+        if value < prev:
+            number -= value
+        else:
+            number = number + value
+        prev = value
+    return number
