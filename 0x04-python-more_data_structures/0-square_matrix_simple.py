a6144530ce3241dc8411cf5507e199939dca3653
meedouazane <mohamedouazane@yahoo.com>
2023-09-13 05:02:41 -0700

    0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/paython3
-def square(x):
-    return x ** 2
-
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
     for i in range(len(matrix)):
-        new_matrix[i] = list(map(square, matrix[i]))
+        new_matrix[i] = list(map(lambda x:x ** 2, matrix[i]))
     return new_matrix
