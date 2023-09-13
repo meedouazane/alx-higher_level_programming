ec2df5a9298cef16e3f19901fd37b283600ef127
meedouazane <mohamedouazane@yahoo.com>
2023-09-13 06:22:40 -0700

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     max_v = 0
     best = ""
-    if a_dictionary: 
+    if a_dictionary:
         for key, value in a_dictionary.items():
             if value > max_v:
                 max_v = value
