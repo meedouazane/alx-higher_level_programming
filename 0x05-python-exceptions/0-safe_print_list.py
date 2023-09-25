e5b464dc5c1b19d50ef07c57036102b17817c591
meedouazane <mohamedouazane@yahoo.com>
2023-09-25 03:26:41 -0700

    0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    try:
+        for i in range(0, x):
+            if my_list[i] == my_list[-1] or x == i + 1:
+                print("{}".format(my_list[i]))
+                c = c + 1
+            else:
+                print("{}".format(my_list[i]), end="")
+                c = c + 1
+        return c
+    except IndexError:
+        return c
