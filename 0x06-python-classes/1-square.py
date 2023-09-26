6dca8ee1c723109d21e3e520f35cf152d60c19b0
meedouazane <mohamedouazane@yahoo.com>
2023-09-26 04:04:20 -0700

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class Square that defines a square """
+
+
+class Square:
+    """  class Square that defines a square by: (based on 0-square.py) """
+
+    def __init__(self, size):
+        """ Private instance attribute: size
+        Instantiation with size (no type/value verification)
+        """
+        self.__size = size
