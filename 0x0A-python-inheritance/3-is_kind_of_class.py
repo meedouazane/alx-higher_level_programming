157c7402a5f1af0fa5873c7d07af517c87e620d9
meedouazane <mohamedouazane@yahoo.com>
2023-10-09 05:46:09 -0700

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' Same class or inherit from '''
+
+
+def is_kind_of_class(obj, a_class):
+    ''' verify if an instance is part of a class or inherted from it'''
+
+    return isinstance(obj, a_class)
