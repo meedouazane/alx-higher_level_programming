0519f6b310ac9cde723c2eb77cc966adaa61c901
meedouazane <mohamedouazane@yahoo.com>
2023-10-09 11:18:36 -0700

    11

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,15 +7,10 @@ class Square(Rectangle):
     ''' Square that inherits from Rectangle (9-rectangle.py) '''
     def __init__(self, size):
         ''' size must be int bigger than 0 '''
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         ''' calculate the area '''
         return self.__size ** 2
-
-    def __str__(self):
-        ''' print the rectangle '''
-        txt = "[Rectangle] "
-        txt += str(self.__size) + "/" + str(self.__size)
-        return txt
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,8 +7,9 @@ class Square(Rectangle):
     ''' Square that inherits from Rectangle (9-rectangle.py) '''
     def __init__(self, size):
         ''' size must be int bigger than 0 '''
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         ''' calculate the area '''
