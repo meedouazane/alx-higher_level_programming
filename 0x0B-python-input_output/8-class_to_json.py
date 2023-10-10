91233853d0d748088a36c7ceacf785690833e4f9
meedouazane <mohamedouazane@yahoo.com>
2023-10-10 07:01:08 -0700

    8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 ''' function that returns the dictionary description
     with simple data structure  '''
-import json
 
 
 def class_to_json(obj):
     ''' returns the dictionary description
     with simple data structure '''
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
