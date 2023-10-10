42a37051af303697059ecf5e2136806eda6aecb7
meedouazane <mohamedouazane@yahoo.com>
2023-10-10 10:55:58 -0700

    11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''  class Student that defines a student '''
+
+
+class Student:
+    '''  class Student that defines a student '''
+
+    def __init__(self, first_name, last_name, age):
+        ''' Public instance attributes:
+        args:
+            first_name:
+            last_name:
+            age:
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        ''' retrieves a dictionary representation of a Student '''
+        if isinstance(attrs, list):
+            if all(isinstance(i, str) for i in attrs):
+                return {elmnt: getattr(self, elmnt)
+                        for elmnt in attrs if hasattr(self, elmnt)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        '''  replaces all attributes of the Student instance '''
+        for key, value in json.items():
+            setattr(self, key, value)
