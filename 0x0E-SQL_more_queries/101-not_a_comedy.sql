862a3ef210e7060f54da102416bc4000c39a1342
meedouazane <mohamedouazane@yahoo.com>
2023-11-15 14:51:44 -0800

    101

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,7 @@
 -- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
 SELECT tv_shows.title AS title
 FROM tv_shows
-JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.genre_id != 5
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL OR tv_show_genres.genre_id != 5
 GROUP BY title
 ORDER BY title;
