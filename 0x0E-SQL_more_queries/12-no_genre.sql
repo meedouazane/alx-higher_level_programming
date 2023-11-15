f2cf27ef30a393192bb762b856fac5e8e7d79294
meedouazane <mohamedouazane@yahoo.com>
2023-11-15 10:51:42 -0800

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 --  lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
-FROM tv_shows RIGHT JOIN tv_show_genres
-ON tv_show_genres.show_id != tv_shows.id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
