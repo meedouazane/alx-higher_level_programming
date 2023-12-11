6eaa7051983143d7850608c27d974e49d3d9dc1a
meedouazane <mohamedouazane@yahoo.com>
2023-12-11 05:16:04 -0800

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const x = +process.argv[2];
+let square = '';
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    for (let y = 0; y < x; y++) {
+      square += 'X' + '';
+    }
+    if (i === x - 1) {
+      break;
+    } else {
+      square += '\n';
+    }
+  }
+}
+console.log(square);
