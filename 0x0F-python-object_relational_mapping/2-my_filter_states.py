6527d87fb72af38ad9a4015bfc2e6404dc5864a7
meedouazane <mohamedouazane@yahoo.com>
2023-12-15 03:09:19 -0800

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-''' safe from MySQL injections! '''
+'''
+    displays all values in the states table of
+    hbtn_0e_0_usa where name matches the argument.
+'''
 import sys
 import MySQLdb
 
@@ -12,7 +15,9 @@ if __name__ == "__main__":
     cur = connection.cursor()
     state_name = sys.argv[4]
     cur.execute("""SELECT * FROM states
-                    WHERE name= %s ORDER BY id ASC""", (state_name,))
+                    WHERE name LIKE BINARY '{}'
+                    ORDER BY id ASC
+                """, .format(state_name))
     rows = cur.fetchall()
     for row in rows:
         print(row)
