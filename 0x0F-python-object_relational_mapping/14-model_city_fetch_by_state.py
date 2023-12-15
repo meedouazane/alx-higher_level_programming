cce86eb0b90d57fe8913de99a5b8c0344169b5c7
meedouazane <mohamedouazane@yahoo.com>
2023-12-15 04:25:48 -0800

    14-model_city_fetch_by_state.

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 '''  prints all City objects from the database hbtn_0e_14_usa '''
 import sys
-from sqlalchemy import create_engine, Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import (create_engine)
 from model_city import Base, City
-from model_state import Base, State
+from model_city import Base, State
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.
+            format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    city = session.query(City)
-    for element in city:
-        print(f"{element.name}: {(element.id)}")
+
+    for C_id, C_name, S_name in session.query(
+            City.id, City.name, State.name).filter(State.id == City.state_id):
+        print(f"{S_name}: ({C_id}) {C_name}")
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 ''' contains the class definition of a State '''
-from sqlalchemy import Column, Integer, String, MetaData, ForeignKey 
+from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
+from model_state import Base, State
 
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
@@ -11,11 +11,9 @@ Base = declarative_base(metadata=mymetadata)
 
 class City(Base):
     ''' inherits from Base '''
-    __tablename__ = 'City'
+    __tablename__ = 'cities'
     id = Column(
             Integer, autoincrement=True, nullable=False,
             unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'))
-    states = relationship('States', backref='City')
-
