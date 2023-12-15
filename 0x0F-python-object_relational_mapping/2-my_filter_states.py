3432b1278c7045d40c4742081aeeef8414d8a3e0
meedouazane <mohamedouazane@yahoo.com>
2023-12-15 02:24:10 -0800

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+'''
+    takes in an argument and displays all values in the states
+    table of hbtn_0e_0_usa where name matches the argument
+'''
+import sys
+import MySQLdb
+
+
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
 
     connection = MySQLdb.connect(
             host="localhost", port=3306, user=sys.argv[1],
