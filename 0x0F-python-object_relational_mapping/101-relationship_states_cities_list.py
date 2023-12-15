0abf06cf2a7b49b241f8772f43b19143ad0e11dc
meedouazane <mohamedouazane@yahoo.com>
2023-12-15 15:28:03 -0800

    102

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" City relationship """
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.
+            format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State, City).filter(State.id == City.state_id).order_by(State.id, City.id)
+    for state, city in query:
+        print(f"{state.id}: {state.name}")
+        for state, city in query:
+            print(f"    {city.id}: {city.name}")
+    session.commit()
