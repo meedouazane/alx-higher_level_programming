43a16ce9a7f9d6e44a2d9a20fe1f9868109fba4d
meedouazane <mohamedouazane@yahoo.com>
2024-01-05 12:26:16 -0800

    6-peak.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,9 +3,12 @@
 
 def find_peak(list_of_integers):
     if list_of_integers:
-        for i in range(len(list_of_integers) - 1):
+        for i in range(1, len(list_of_integers) - 1):
             if list_of_integers[i] >= list_of_integers[i + 1] and list_of_integers[i] >= list_of_integers[i - 1]:
-                peak = list_of_integers[i]
-                return peak
+                return list_of_integers[i]
+        if list_of_integers[0] >= list_of_integers[1]:
+            return list_of_integers[0]
+        if list_of_integers[-1] >= list_of_integers[-2]:
+            return list_of_integers[-1]
     else:
         return None
