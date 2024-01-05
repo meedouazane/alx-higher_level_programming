9177130cbc9344ca3910b513aa409d835d0d0f50
meedouazane <mohamedouazane@yahoo.com>
2024-01-05 04:28:09 -0800

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# cURL body size
-curl -L "$1"
+# Display only body of a 200 status code response
+status=$(curl -sI 0.0.0.0:5000 | grep -i HTTP/1.1 | cut -d' ' -f2)
+if [ "$status" -eq 200 ]; then
+        curl -L "$1"
+fi
