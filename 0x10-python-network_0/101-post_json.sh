a03eee3c3b220bb808e563eb137df21a8bf23858
meedouazane <mohamedouazane@yahoo.com>
2024-01-05 10:52:36 -0800

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument
 json_file="$2"
-curl -X POST -s --data-ascii "$1" "@$json_file"
+curl -X POST -s --data-ascii "@$json_file" "$1"
