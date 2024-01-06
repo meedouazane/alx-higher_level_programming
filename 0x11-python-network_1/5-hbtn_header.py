0cd4892d8c22d5016fa890186f08e21224fe97fc
meedouazane <mohamedouazane@yahoo.com>
2024-01-06 04:32:13 -0800

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 ''' displays the value of the variable X-Request-Id '''
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
+    url = sys.argv[1]
     content = requests.get(url)
     if 'X-Request-Id' in content.headers:
         print(content.headers['X-Request-Id'])
