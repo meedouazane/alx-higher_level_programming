2a579f409623b6f021f824f891504484d08a10b9
meedouazane <mohamedouazane@yahoo.com>
2024-01-06 03:31:34 -0800

    all

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' Python script that fetches website '''
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print(f"\t - type: {type(content)}")
+        print(f"\t - content: {content}")
+        print(f"\t - utf8 content: {content.decode('utf-8')}")
