627cd41da205b8d33f2bec4c99559523ec6ec96a
meedouazane <mohamedouazane@yahoo.com>
2024-01-06 05:30:38 -0800

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 ''' manage error exceptions and print: Error code '''
-
-
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
-    request = requests.get(argv[1])
-    if request.status_code >= 400:
-        print("Error code: {}".format(request.status_code))
+    url = argv[1]
+    content = requests.get(url)
+    if content.status_code >= 400:
+        print(f"Error code: {content.status_code}")
     else:
-        print(request.text)
+        print(content.text)
