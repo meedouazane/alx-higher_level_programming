356f17bde7c491e06a4f11f2a24f7725080112d7
meedouazane <mohamedouazane@yahoo.com>
2024-01-06 06:13:46 -0800

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' script that takes in a letter and sends a POST request '''
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    try:
+        variable = {'q': sys.argv[1]}
+    except IndexError:
+        variable = {'q': ''}
+    req = requests.post(url, data=variable)
+    data = req.json()
+    if data:
+        print(f"[{data['id']}] {data['name']}")
+    else:
+        print("No result")
