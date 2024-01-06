9a2554fd7ad53745009fb544878c4e628ab7b318
meedouazane <mohamedouazane@yahoo.com>
2024-01-06 05:49:07 -0800

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     content = requests.get(url)
     print("Body response:")
-    print(f"\t - type: {type(content.text)}")
-    print(f"\t - content: {content.text}")
+    print("\t - type: {}".format(type(content.text)))
+    print("\t - content: {}".format(content.text))
