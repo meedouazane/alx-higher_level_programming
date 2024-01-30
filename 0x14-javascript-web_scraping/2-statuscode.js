a51464af3467d6b5a4ecd7d79db2321f33384ee5
Ouazane <mohamedouazane@yahoo.com>
2024-01-30 02:18:58 -0800

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const arg = process.argv;
+request(arg[2], function (error, response) {
+  if (error) {
+    console.error(error.message);
+  }
+  console.log('code: ', response && response.statusCode);
+});
